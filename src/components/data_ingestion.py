3e435ee6d54b2eff9b8aa6ba2d2130a8afde0bf3
AAYUSH KUMAR <22it3001@rgipt.ac.in>
2025-01-13 00:11:54 +0530

    pkl model formation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,11 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -52,12 +57,10 @@ class DataIngestion:
 
 
 if __name__ == "__main__":
-    try:
-        obj = DataIngestion()
-        train_path, test_path = obj.initiate_data_ingestion()
-        logging.info(f"Data Ingestion completed. Train Path: {train_path}, Test Path: {test_path}")
-    except Exception as e:
-        logging.error(f"Error in main: {str(e)}")
-
-
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
